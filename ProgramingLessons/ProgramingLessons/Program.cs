30ac1784b3a4b5cd47088ee6e6c283e15456b1d3
BachoWebLpc\GtppLpc <bacho1988@gmail.com>
2025-10-23 19:54:46 +0400

    პირველი გაკვეთილი

--- ProgramingLessons/ProgramingLessons/Program.cs
+++ ProgramingLessons/ProgramingLessons/Program.cs
@@ -8,31 +8,55 @@ namespace ProgramingLessons
         // Entry point
         static void Main(string[] args)
         {
-            // --- your code here ---
-            int age = 32;
-            double price = 19.99;
-            decimal salary = 75000.50m;
-            bool isActive = true;
-            char grade = 'A';
-            string name = "Maya";
+            // --- Variables and dataTypes ---
+            //int age = 32;
+            //double price = 19.99;
+            //decimal salary = 75000.50m;
+            //bool isActive = true;
+            //char grade = 'A';
+            //string name = "Elene";
 
-            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade}");
-            Console.WriteLine($"Price: {price}, Salary: {salary}, Active: {isActive}");
+            //Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade}");
+            //Console.WriteLine($"Price: {price}, Salary: {salary}, Active: {isActive}");
 
-            int[] scores = new[] { 90, 85, 92 };
-            var names = new List<string> { "Ana", "Ben" };
-            Console.WriteLine($"First score: {scores[0]}, First name: {names[0]}");
+            //int[] scores = new[] { 90, 85, 92 };
+            //var names = new List<string> { "Ana", "Ben" };
+            //Console.WriteLine($"First score: {scores[0]}, First name: {names[0]}");
 
-            // keep window open (optional)
-            // Console.ReadLine();
+            //// keep window open (optional)
+            //// Console.ReadLine();
 
 
-            Console.Write("Enter your name: ");
-            string? name2 = Console.ReadLine();
+            //Console.Write("Enter your name: ");
+            //string? name2 = Console.ReadLine();
 
-            Console.WriteLine(name2);
+            //Console.WriteLine(name2);
 
-            Console.ReadKey();
+            //Console.ReadKey();
+
+
+            //მონაცემები შეყვანა კონსოლიდან
+            string label = "Sheikvanet ricxvi:";
+
+            Console.WriteLine(label);
+
+            string lavelvalue = Console.ReadLine();
+
+            Console.WriteLine(label);
+
+            //მათემატიკური ოპერაციები 
+
+            //ლოგიკური ოპერატორები
+
+            //პირობითი ოპერატორები if else, switch case
+
+            //ციკლები for, while, do wtile, foreach => ამას განვიხილავთ მოგვიანებით 
+
+            //მასივები 
+
+            //ფუქნციები
+
+            //კლასები
         }
     }
 }
