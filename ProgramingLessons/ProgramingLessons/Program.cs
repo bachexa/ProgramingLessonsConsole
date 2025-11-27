22bd47afcaf443d3e713b1394205ef5639ff8efa
BachoWebLpc\GtppLpc <bacho1988@gmail.com>
2025-11-27 20:36:54 +0400

    მონაცემების შეყვანა კონსოლიდან

--- ProgramingLessons/ProgramingLessons/Program.cs
+++ ProgramingLessons/ProgramingLessons/Program.cs
@@ -42,8 +42,16 @@ namespace ProgramingLessons
 
 
 
-            //მონაცემთა ტიპები
-            int age = 32;
+            //1
+            //int x = 1598;
+            //int y = 15478;
+
+            //int z = x + y;
+
+            //string shedegi = $"{x} + {y} = {z}";
+
+            //Console.WriteLine(shedegi);
+
 
 
             //double price = 19.99;
@@ -53,30 +61,84 @@ namespace ProgramingLessons
             //string name = "Elene elene bacho elene bacho dani ";
 
 
-            Console.WriteLine(age);
-           // Console.ReadKey();
+            //Console.WriteLine(age);
+            // Console.ReadKey();
 
-           
+
+
+
+
+
+
+
+            //მონაცემები შეყვანა კონსოლიდან გადაკონვერტირება სხვადასხვა ტიპებში და მათემატიკური ოპერაციები
+            string label = "Sheikvanet pirveli ricxvi:";
+
+            Console.WriteLine(label);
+
+            string monacemi1 = Console.ReadLine();
+
+            int intMonacemi1 = Convert.ToInt32(monacemi1);
+
+            string label2 = "sheikvanet meore ricxvi";
+
+            Console.WriteLine(label2);
+
+            string monacemi2 = Console.ReadLine();
+
+            int intMonacemi2 =  Convert.ToInt32(monacemi2);
+
+            string jami = monacemi1 + monacemi2;
+
+            int jamiInt = intMonacemi1 + intMonacemi2;
+
+            int sxvaoba = intMonacemi1 - intMonacemi2;
+
+            int gamravleba = intMonacemi1 * intMonacemi2;
+
+            int ganakopiInt = intMonacemi1 / intMonacemi2;
+
+         
+
             
-            
+
+            Console.WriteLine(monacemi1 + " + " + monacemi2 + " = " + jami);
+
+            Console.WriteLine(intMonacemi1 + " + " + intMonacemi2 + " = " + jamiInt);
+
+            Console.WriteLine(intMonacemi1 + " - " + intMonacemi2 + " = " + sxvaoba);
+
+            Console.WriteLine(intMonacemi1 + " * " + intMonacemi2 + " = " + gamravleba);
+
+            Console.WriteLine(intMonacemi1 + " / " + intMonacemi2 + " = " + ganakopiInt);
+
+
+
+       /*გავასწოროთ შემდეგი შეცდომები 
+        * 1.როცა წილადი რიცხვი შემყავს შედომა რომ არ გამოიტანოს 
+        * 2 როცა გაყოფა ხდება მომცეს სრული წილადი რიცხვი არსებობის შემთხვევაში 
+        
+        
+        */
 
 
 
 
-            //მონაცემები შეყვანა კონსოლიდან
-            //string label = "Sheikvanet ricxvi:";
 
-            // Console.WriteLine(label);
 
-            // string lavelvalue = Console.ReadLine();
 
-            //Console.WriteLine(label);
+
+
+
+
 
             //მათემატიკური ოპერაციები 
 
+            //პირობითი ოპერატორები if else, switch case
+
             //ლოგიკური ოპერატორები
 
-            //პირობითი ოპერატორები if else, switch case
+
 
             //ციკლები for, while, do wtile, foreach => ამას განვიხილავთ მოგვიანებით 
 
